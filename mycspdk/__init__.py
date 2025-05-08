50eb5a3d4bec96f9910ad57677b2353c1cef09b1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-05-08 15:32:59 -0700

    fix pdk

--- mycspdk/__init__.py
+++ mycspdk/__init__.py
@@ -4,9 +4,3 @@ from cspdk.si220 import PDK
 from doroutes.bundles import add_bundle_astar
 
 PDK.routing_strategies["doroute_astar"] = add_bundle_astar
-
-from mycspdk import drc_errors as drc_errors
-from mycspdk import lvs_electrical as lvs_electrical
-from mycspdk import mzis as mzis
-from mycspdk import nxn as nxn
-
