688ce664b9fd31b1ac8206d9232525a85639cbf0
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-05-14 18:58:04 -0700

    new bundle sample

--- mycspdk/splitter_tree.py
+++ mycspdk/splitter_tree.py
@@ -2,6 +2,6 @@ import gdsfactory as gf
 
 
 @gf.cell
-def splitter_tree(cross_section="xs_sc", **kwargs) -> gf.Component:
+def splitter_tree(cross_section="strip", **kwargs) -> gf.Component:
     c = gf.c.splitter_tree(cross_section=cross_section, **kwargs)
     return c
