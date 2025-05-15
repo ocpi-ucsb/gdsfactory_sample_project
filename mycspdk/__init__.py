7be0f8faf99152c4e2a88797b978158be95cdc0d
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-05-14 19:02:42 -0700

    fix astar

--- mycspdk/__init__.py
+++ mycspdk/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.0"
 
-from cspdk.si220 import PDK
+from cspdk.si220.cband import PDK
 from doroutes.bundles import add_bundle_astar
 
 PDK.routing_strategies["doroute_astar"] = add_bundle_astar
