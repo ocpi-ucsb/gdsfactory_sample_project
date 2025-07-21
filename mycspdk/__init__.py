88b7d4112e3748400c206a5da4b94ea8b5433a09
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-07-21 10:53:06 +0200

    add path

--- mycspdk/__init__.py
+++ mycspdk/__init__.py
@@ -1,1 +1,13 @@
 __version__ = "0.0.0"
+
+import pathlib
+
+from cspdk.si220.cband import PDK
+
+home = pathlib.Path.home()
+cwd = pathlib.Path.cwd()
+module_path = pathlib.Path(__file__).parent.absolute()
+repo_path = module_path.parent
+
+
+__all__ = ["PDK"]
