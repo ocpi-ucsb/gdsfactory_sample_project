6e0583d6b626a133d82ce7ccb088ec51ebf822be
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-07-24 06:11:26 -0400

    fix pre-commit

--- mycspdk/cross_sections/__init__.py
+++ mycspdk/cross_sections/__init__.py
@@ -1,7 +1,4 @@
-from collections.abc import Callable
-from typing import Any
-
-from .demo import strip600
+from .strip600 import strip600
 
 cross_sections = {}
 cross_sections["strip600"] = strip600
